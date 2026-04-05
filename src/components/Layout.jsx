f811bf8fe24130bacb9f87f9b39d1d05d029a445
Sebastian Cruz <sebalox92@gmail.com>
2026-04-04 23:57:35 -0400

    initial app layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,45 @@
+import classes from './layout.module.css';
+import { useState } from 'react';
+
+
+const Layout = () => {
+    const [data, setData] = useState([]);
+    const [fail, setFail] = useState(null);
+    const [input, setInput] = useState("");
+
+    const fecthData = async () => {
+
+        try {
+            const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API}&q=london&days=7&aqi=no&alerts=no`)
+            const weather = await response.json();
+            console.log(weather)
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fecthData();
+
+
+    }
+
+    return (
+        <main className={classes.container} onSubmit={handleSubmit}>
+            <h1>Weather App</h1>
+            <form>
+                <p>Enter city name</p>
+                <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+                <button type='submit'>Get Weather Data</button>
+            </form>
+            <p>{fail}</p>
+            
+
+
+        </main>
+    )
+}
+
+export default Layout;
