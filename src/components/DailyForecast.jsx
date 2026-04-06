a89341a7cf5431d22e28aeca18a399a42a11f477
Sebastian Cruz <sebalox92@gmail.com>
2026-04-05 23:10:08 -0400

    center forecast img

--- src/components/DailyForecast.jsx
+++ src/components/DailyForecast.jsx
@@ -4,7 +4,7 @@ import classes from './dailyforecast.module.css';
 const DailyForecast = ({day ,avgtemp_c, avgtemp_f, mintemp_c, mintemp_f, maxtemp_c, maxtemp_f, icon, text}) => {
     return (
         <div className={classes.dailyForecast}>
-            <p>{day}</p>
+            <p className={classes.day}>{day}</p>
             <p><b>AVG: </b>{avgtemp_c}℃ / {avgtemp_f}℉</p>
             <p><b>Low: </b>{mintemp_c}℃ / {mintemp_f}℉</p>
             <p><b>High: </b>{maxtemp_c}℃ / {maxtemp_f}℉</p>
